748df12806badd4b8b300f0f630d445086bdc191
AnnoyedSandwich <41872159+AnnoyedSandwich@users.noreply.github.com>
2023-08-24 03:25:05 +0200

    restructure

---
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/darq/encoder.c
@@ -0,0 +1,43 @@
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
+
+#if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+
+};
+#endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+  if (index == 0) { /* First encoder */
+    if (clockwise) {
+      tap_code(KC_PGDN);
+    } else {
+      tap_code(KC_PGUP);
+    }
+  } else if (index == 1) { /* Second encoder */
+    if (clockwise) {
+      if (!is_alt_tab_active) {
+        is_alt_tab_active = true;
+        register_code(KC_LALT);
+      }
+      alt_tab_timer = timer_read();
+      tap_code16(KC_TAB);
+    } else {
+      if (!is_alt_tab_active) {
+        is_alt_tab_active = true;
+        register_code(KC_LALT);
+      }
+      alt_tab_timer = timer_read();
+      tap_code16(S(KC_TAB));
+    }
+  }
+  return false;
+}
+void matrix_scan_user(void) {
+  if (is_alt_tab_active) {
+    if (timer_elapsed(alt_tab_timer) > 800) {
+      unregister_code(KC_LALT);
+      is_alt_tab_active = false;
+    }
+  }
+}
